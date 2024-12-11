1b052d757724d53e7d19a02416f3c992a15565b6
bekesi142 <bekesibalint2006@gmail.com>
2024-12-11 14:21:15 +0100

    haladas 1211

--- QuizWindow.xaml.cs
+++ QuizWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,23 @@ namespace szotanulo_alkalmazas
         {
             InitializeComponent();
 
+            List<string> angolSzavak = new List<string>();
+            List<string> magyarSzavak = new List<string>();
+
+            StreamReader sr = new StreamReader("eng_hun.txt");
+            while (!sr.EndOfStream)
+            {
+                string[] felbontottSor = sr.ReadLine().Split(" - ");
+                angolSzavak.Add(felbontottSor[0]);
+                magyarSzavak.Add(felbontottSor[1]);
+            }
+            sr.Close();
+
+            /*
+            string filepath = "eng_hun.txt";
+            List<Szo> szoparok = new List<Szo>();
+            File.ReadAllLines("filepath").ToList().ForEach(x => szoparok.Add(new Szo(x))); */
+
             // A kiválasztott nehézségi szint alapján döntsük el, hány StackPanel-t kell hozzáadni
             int numberOfPanels = 0;
 
@@ -42,15 +60,26 @@ namespace szotanulo_alkalmazas
             // StackPanel-ek dinamikus létrehozása
             for (int i = 0; i < numberOfPanels; i++)
             {
-                var stackPanel = new StackPanel { Orientation = Orientation.Vertical, Margin = new Thickness(10) };
+
+                var stackPanel = new StackPanel { Orientation = Orientation.Vertical, Margin = new Thickness(10)};
 
                 // Label
-                var label = new Label { Content = "Hello", HorizontalAlignment = HorizontalAlignment.Center };
+                Random random = new Random();
+                string randomMagyarszo = magyarSzavak[random.Next(0, magyarSzavak.Count -1)];
+                var label = new Label { Content = randomMagyarszo, HorizontalAlignment = HorizontalAlignment.Center };
                 stackPanel.Children.Add(label);
 
                 // Gombok (1, 2, 3, 4)
+
+                List<string> valaszok = new List<string>();
                 var buttonPanel = new StackPanel { Orientation = Orientation.Horizontal, HorizontalAlignment = HorizontalAlignment.Center };
-                for (int j = 1; j <= 4; j++)
+                for (int j = 1; j <= 3; j++)
+                {
+                    
+                    
+                }
+
+                for (int j = 0; j < valaszok.Count; j++)
                 {
                     var button = new Button { Content = j.ToString(), Width = 50, Margin = new Thickness(5) };
                     buttonPanel.Children.Add(button);
---
+++ Szo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szotanulo_alkalmazas
+{
+    internal class Szo
+    {
+        string magyarSzo, angolSzo;
+
+        public Szo(string sor)
+        {
+            var mezo = sor.Split(" - ");
+            this.angolSzo = mezo[0];
+            this.magyarSzo = mezo[1];
+        }
+
+        public string MagyarSzo { get => magyarSzo; set => magyarSzo = value; }
+        public string AngolSzo { get => angolSzo; set => angolSzo = value; }
+    }
+}
